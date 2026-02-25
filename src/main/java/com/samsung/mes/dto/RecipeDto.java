77eb34628f26651700717c9b8a1cf9a3d4ea10ed
jiwonsongg1-ops <jiwonsong.g1@gmail.com>
2026-02-25 12:48:06 +0900

    recipe CRUD and dummy data

--- src/main/java/com/samsung/mes/dto/RecipeDto.java
+++ src/main/java/com/samsung/mes/dto/RecipeDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class RecipeDto {
+public class RecipeDTO {
     private Long id;
     private String name;
     private String description;
